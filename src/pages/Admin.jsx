3b20bd4851bb4fe9a36b02dea00ba5516738aa8b
erpanter <nicholashosw06@gmail.com>
2026-07-18 02:07:34 +0800

    testing admin backend

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -25,6 +25,7 @@ export default function Admin({
   // =========================
   // ADD / UPDATE PROJECT
   // =========================
+  console.log(updatedProject);
   const handleSaveProject = async () => {
 
     try {
